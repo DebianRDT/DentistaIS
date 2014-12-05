017878de90d2bb7c29b82114003e296c357b9325
Ignacio <ignacio@ignacio-SATELLITE-L850-150.(none)>
2014-12-05 10:25:52 +0100

    cambiar atributo

--- clinica/paciente.cpp
+++ clinica/paciente.cpp
@@ -40,7 +40,26 @@ const std::list<AtributoMultiple*>& Paciente::lista_atributos_multiples(const Ti
 /** Cambia el atributo multiple en la posicion n, por el nuevo
  *
  */
-bool Paciente::cambiar_atributo(int n, const AtributoMultiple& nuevo){}
+bool Paciente::cambiar_atributo(int n, const AtributoMultiple& nuevo){
+  
+  list<AtributoMultiple>::iterator pos;
+  
+  bool atributo;
+  int i=0;
+  for(pos=_atributos_multiples_.begin();pos!=_atributos_multiples_.end();pos++)
+  {
+     
+     if(i==n-1)
+      {
+        *pos=nuevo;    
+        atributo=true;
+        
+      }
+    i++; 
+      
+  }
+
+}
 
 
 /** Agrega atributo multiple al final de la lista
