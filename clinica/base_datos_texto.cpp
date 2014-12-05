c5d4bca7917455346acd94939abf6b97086d5475
Debian <Debianzonerdt@gmail.com>
2014-12-05 11:04:38 +0100

    Ampliadas las clases de bases de datos y paciente

--- clinica/base_datos_texto.cpp
+++ clinica/base_datos_texto.cpp
@@ -23,6 +23,34 @@ std::list<Paciente*> BaseDatosTexto::cargar_desde(std::string n_fichero) {
 }
 
 bool BaseDatosTexto::guardar() {
+    char * nombreFichero="datosAlmacenados.txt";
+    ofstream fichero (nombreFichero);
+
+    list<Paciente*>::iterator i;
+    for(i=_todos.begin(); i!=_todos.end(); i++) {
+        fichero<<(*i).get_dni();
+        fichero<<",";
+        fichero<<(*i).get_apellido1();
+        fichero<<",";
+        fichero<<(*i).get_apellido2();
+        fichero<<",";
+        fichero<<(*i).get_nombre();
+        fichero<<",";
+        fichero<<(*i).get_fecha_nacimiento();
+        fichero<<",";
+        fichero<<(*i).get_sexo();
+        fichero<<",";
+        fichero<<(*i).get_frecuencia;
+        list<AtributoMultiple*>::iterator j;
+        for(j=_atributos_multiples.begin(); j != _atributos_multiples.end(); j++) {
+            fichero<<(*j).tipo;
+            fichero<<"(";
+            fichero<<(*j).contenido;
+            fichero<<")";
+        }
+        fichero<<endl;
+    }
+    fichero.close();
 }
 
 bool BaseDatosTexto::guardar_en(std::string n_fichero) {
