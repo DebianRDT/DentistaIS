0a126da2809c02cc954207232464b3da3f63729a
Debian <Debianzonerdt@gmail.com>
2014-12-15 02:06:20 +0100

    Añadidos nuevos tests a paciente

--- src/test/paciente_unitest.cc
+++ src/test/paciente_unitest.cc
@@ -5,17 +5,41 @@
 TEST(Paciente, constructor) {
     Paciente p("Apellido1", "Apellido2");
 
-    EXPECT_EQ("Apellido1", p.getApellido1());
-    EXPECT_EQ("Apellido2", p.getApellido2());
+    EXPECT_EQ("Apellido1", p.get_Apellido1());
+    EXPECT_EQ("Apellido2", p.get_Apellido2());
 
 }
 
 TEST(Paciente, asignar_apellidos) {
     Paciente p("Apellido1", "Apellido2");
-    p.setApellido1("LastName1");
-    p.setApellido2("LastName2");
+    p.set_Apellido1("LastName1");
+    p.set_Apellido2("LastName2");
 
-    EXPECT_EQ("LastName1", p.getApellido1());
-    EXPECT_EQ("LastName2", p.getApellido2());
+    EXPECT_EQ("LastName1", p.get_Apellido1());
+    EXPECT_EQ("LastName2", p.get_Apellido2());
 
 }
+
+TEST(Paciente, asignar_nombre) {
+    Paciente p("Apellido1", "Apellido2");
+
+    p.set_Nombre("Paco");
+    EXPECT_EQ("Paco", p.get_Nombre());
+
+    p.set_Nombre("Juan");
+    EXPECT_EQ("Juan", p.get_Nombre());
+}
+
+TEST(Paciente, favorito) {
+    Paciente p("Apellido1", "Apellido2");
+
+    EXPECT_EQ(false, p.get_Favorito());
+
+    p.set_Favorito();
+    EXPECT_EQ(true, p.get_Favorito());
+
+    p.reset_Favorito();
+    EXPECT_EQ(false, p.get_Favorito());
+}
+
+TEST(Paciente, asignar_apellidos)
