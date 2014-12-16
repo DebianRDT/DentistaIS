239a13005af82d90d35f809ce2b7157b2a6cffd6
Debian <Debianzonerdt@gmail.com>
2014-12-16 21:09:34 +0100

    Empezado el test de contacto, lo continuo mañana

--- src/test/contacto_unittest.cc
+++ src/test/contacto_unittest.cc
@@ -1,2 +1,9 @@
 #include "../contacto.h"
 #include "gtest/gtest.h"
+
+
+TEST(Contacto, contar_atributo_multiple) {
+    Paciente c("Apellido1", "Apellido2");
+
+    EXPECT_EQ(0, c.contar_atributo_multiple());
+}
