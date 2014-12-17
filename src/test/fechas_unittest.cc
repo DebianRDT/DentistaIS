3830902926d3c2cfadc15fe1164509ae6f6ef5ea
Francisco J. Jiménez Álvarez <franciscojesusjimenez@gmail.com>
2014-12-17 16:54:13 +0100

    fechas unittest

---
+++ src/test/fechas_unittest.cc
@@ -0,0 +1,24 @@
+#include "../fechas.h"
+#include "gtest/gtest.h"
+
+using namespace agenda;
+
+
+
+TEST(Fechas, gestion_fechas) {
+    Fecha f(20,10,2010);
+
+    EXPECT_EQ(true, f.validar());
+
+    f.setFecha(32,10,2010);
+    EXPECT_EQ(true, f.validar());
+
+    EXPECT_EQ(false, f.validar(29,2,2015));
+
+    f.setFecha(29,2,2012);
+    EXPECT_EQ(true, f.validar());
+
+    f.setFecha(1,1,2011);
+    EXPECT_EQ("1/1/2011", f.getFecha());
+
+}
