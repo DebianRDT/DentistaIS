22cd748d1520ed2fa2f6c6bb2f1ca2376767676f
Francisco J. Jiménez Álvarez <franciscojesusjimenez@gmail.com>
2015-01-07 14:58:29 +0100

    imprimir paciente

--- src/paciente.cpp
+++ src/paciente.cpp
@@ -10,14 +10,16 @@ namespace agenda{
    *  Es obligatorio definirla para todas las derivadas de Contacto
    */
   void Paciente::_print(std::ostream& o){
-    o<<_nombre<<std::endl;
-    o<<_apellido1<<std::endl<<_apellido2<<std::endl;
+    o<<_apellido1<<" "<<_apellido2<<", "<<_nombre<<" DNI:"<<_dni<<std::endl;
+    o<<_sexo<<". Nacimiento: "<<_fecha_nacimiento<<std::endl;
 
     //imprimo atributos multiples
-    o<<"INFORMACION EXTRA ("<<contar_atributos_multiples()<<")"<<std::endl;
+    o<<"+ INFO ("<<contar_atributos_multiples()<<")"<<std::endl;
     while(!es_ultimo()){
       o<<*get_atributo_multiple()<<std::endl;
     }
+
+    o<<"---"<<std::endl;
 
   }
 
@@ -104,8 +106,8 @@ namespace agenda{
     add_atributo_multiple(n);
 
   }
-  
-  
+
+
   void  Paciente::add_direccion(const std::string& direccion){
     Atributo* d = new Atributo;
     d->set_contenido(direccion);
@@ -115,7 +117,7 @@ namespace agenda{
 
   }
 
-  
+
   void  Paciente::add_red_social(const std::string& red_social){
     Atributo* r = new Atributo;
     r->set_contenido(red_social);
@@ -124,7 +126,7 @@ namespace agenda{
     add_atributo_multiple(r);
 
   }
-  
+
 
 
 
