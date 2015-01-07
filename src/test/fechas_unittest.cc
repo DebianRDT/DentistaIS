61562317da7900c2c39cccb35669708a3c710e94
Jose Molino Ortega <debianzonerdt@gmail.com>
2015-01-07 17:04:27 +0100

    modificacion en fecha

--- src/test/fechas_unittest.cc
+++ src/test/fechas_unittest.cc
@@ -7,18 +7,15 @@ using namespace agenda;
 
 TEST(Fechas, gestion_fechas) {
     Fecha f(20,10,2010);
+    EXPECT_EQ("20/10/2010", f.getFecha());
 
-    EXPECT_EQ(true, f.validar());
-
-    f.setFecha(32,10,2010);
-    EXPECT_EQ(true, f.validar());
-
-    EXPECT_EQ(false, f.validar(29,2,2015));
-
-    f.setFecha(29,2,2012);
-    EXPECT_EQ(true, f.validar());
+    Fecha f1("20/11/2012");
+    EXPECT_EQ("20/11/2012", f1.getFecha());
 
     f.setFecha(1,1,2011);
     EXPECT_EQ("1/1/2011", f.getFecha());
+
+    f.setFecha("2/2/2012");
+    EXPECT_EQ("2/2/2012", f.getFecha());
 
 }
